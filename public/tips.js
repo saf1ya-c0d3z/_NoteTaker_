9e470c30c1d9039c3ed90fbbe2945b90cf315776
Safiya Shrader <saf1ya.c0d3z@gmail.com>
2023-04-04 12:45:00 -0700

    add js for saving note

---
+++ public/tips.js
@@ -0,0 +1,36 @@
+const fb = require('express').Router();
+const { v4: uuidv4 } = require('uuid');
+const { readAndAppend, readFromFile } = require('../helpers/fsUtils');
+
+// GET Route for retrieving all the feedback
+fb.get('/', (req, res) =>
+  readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)))
+);
+
+// POST Route for submitting feedback
+fb.post('/', (req, res) => {
+  // Destructuring assignment for the items in req.body
+  const { email, feedbackType, feedback } = req.body;
+
+  // If all the required properties are present
+  if (title && text) {
+    // Variable for the object we will save
+    const newFeedback = {
+      title,
+      text,
+    };
+
+    readAndAppend(newFeedback, './db/db.json);
+
+    const response = {
+      status: 'success',
+      body: newFeedback,
+    };
+
+    res.json(response);
+  } else {
+    res.json('Error in posting feedback');
+  }
+});
+
+module.exports = fb;
