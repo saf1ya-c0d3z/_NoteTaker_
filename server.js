a348ae78e3ad241a5fcebd98a1ee5439b57d60b1
saf1ya-c0d3z <118941179+saf1ya-c0d3z@users.noreply.github.com>
2023-04-10 17:59:02 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const db = require('./db/db.json');
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const app = express();
 const api = require('./routes')
 
